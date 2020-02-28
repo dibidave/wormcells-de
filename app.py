483527582a7df8f920beb3d7b99b224e2a6bf2e9
munfred <munfred@brandeis.edu>
2020-02-28 00:22:24 -0800

    add email monitoring!

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ def receive_submission():
     client = boto3.client('s3',
                           aws_access_key_id=AWS_S3_ACCESS_KEY,
                           aws_secret_access_key=AWS_S3_SECRET,
-                          # region_name='us-east-2'
+                          region_name='us-east-2'
                           )
     client.put_object(
         Body=csv_buffer.getvalue(),
